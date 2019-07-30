072948e233d8f5933e772e16bf57b469d3d9d282
Cuong Desktop <nguyenc72694@gmail.com>
2019-07-30 13:33:40 -0500

    game is almost done. The questions are repeating so not sure how to chagne that

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# TriviaGame
+TriviaGame
+
+Overview 
+
+Created a Pokemon Trivia game using Javascript, jQuery, HTML and CSS. There are 6 questions randomly generated and a countdown for the questions. Good luck! 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,5 @@
-//time remaining displays and counts down 
-//when time is over the game is over 
-//questions are displayed 
-//you can only choose one option at a time 
-//if you can answer it all you can click done
-
-//end of game 
-//displays counts 
-//ALL DONE
-//Correct answers
-//incorrect answers 
-//unanswered  
-
 $(document).ready(function () {
-    var questionpool = [
+    var options = [
         {
             question: "What's the most effective Poke ball in the game?",
             choice: ["Great Ball", "Ultra Ball", "Heal Ball", "Master Ball"],
@@ -49,6 +36,13 @@ $(document).ready(function () {
     var unanswered = 0;
     var timer = 15;
     var intervalId;
+    var userGuess = "";
+    var running = false;
+    var qCount = options.length;
+    var pick;
+    var index;
+    var holder = [];
+
 
     //start game 
     $("#startOver").hide();
@@ -58,17 +52,119 @@ $(document).ready(function () {
         //on click the button disappears
         $("#start").hide();
         //quiz shows up and timer starts
-        displayQuestion();
         runTimer();
+        displayQuestion();
         for (var i = 0; i < options.length; i++) {
             holder.push(options[i]);
         }
     })
 
+    //timer start
+    function runTimer() {
+        if (!running) {
+            intervalId = setInterval(decrement, 1000);
+            running = true;
+        }
+    }
 
+    //timer countdown
+    function decrement() {
+        $("#timer").html("<h3>Time remaining: " + timer + "</h3>");
+        timer--;
 
+        //stop timer if reach 0
+        if (timer === 0) {
+            unanswered++;
+            stop();
+            $("#answer").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            nextquestion();
+        }
+    }
 
+    //timer stop
+    function stop() {
+        running = false;
+        clearInterval(intervalId);
+    }
+    //randomly pick question in array if not already shown
+    //display question and loop though and display possible answers
+    function displayQuestion() {
+        //generate random index in array
+        index = Math.floor(Math.random() * options.length);
+        pick = options[index];
 
+        //iterate through answer array and display
+        $("#question").html("<h2>" + pick.question + "</h2>");
+        for (var i = 0; i < pick.choice.length; i++) {
+            var userChoice = $("<div>");
+            userChoice.addClass("answerchoice");
+            userChoice.html(pick.choice[i]);
+            //assign array position to it so can check answer
+            userChoice.attr("data-guessvalue", i);
+            $("#answer").append(userChoice);
+        }
 
+        //click function to select answer and outcomes
+        $(".answerchoice").on("click", function () {
+            //grab array position from userGuess
+            userGuess = parseInt($(this).attr("data-guessvalue"));
 
+            //correct guess or wrong guess outcomes
+            if (userGuess === pick.answer) {
+                stop();
+                correctCount++;
+                userGuess = "";
+                $("#answer").html("<p>Correct!</p>");
+                nextquestion();
+
+            } else {
+                stop();
+                wrongCount++;
+                userGuess = "";
+                $("#answer").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+                nextquestion();
+            }
+        })
+    }
+    function nextquestion() {
+        //run the score screen if all questions answered
+        //end of game 
+        //displays counts 
+        //Correct answers
+        //incorrect answers 
+        //unanswered
+        //resets timer for each new question
+        var newtime = setTimeout(function () {
+            $("#answer").empty();
+            timer = 15;
+
+            if ((wrongCount + correctCount + unanswered) === qCount) {
+                $("#question").empty();
+                $("#question").html("<h3>Game Over!  Here's how you did: </h3>");
+                $("#answer").append("<h4> Correct: " + correctCount + "</h4>");
+                $("#answer").append("<h4> Incorrect: " + wrongCount + "</h4>");
+                $("#answer").append("<h4> Unanswered: " + unanswered + "</h4>");
+                $("#startOver").show();
+                correctCount = 0;
+                wrongCount = 0;
+                unanswered = 0;
+
+            } else {
+                runTimer();
+                displayQuestion();
+            }
+        }, 2000);
+    }
+
+    //starts over and starts another game.
+    $("#startOver").on("click", function () {
+        $("#startOver").hide();
+        $("#answer").empty();
+        $("#question").empty();
+        for (var i = 0; i < holder.length; i++) {
+            options.push(holder[i]);
+        }
+        runTimer();
+        displayQuestion();
+    })
 });
